6d6eed7083fcd1037518e7a7b0eeceed2fd111e1
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2021-04-09 15:05:18 -0500

    Config.txt fixes and jar file
    Adds executable jar file that creates editable config.txt file

--- src/Config/ConfigReader.java
+++ src/Config/ConfigReader.java
@@ -6,21 +6,36 @@ package Config;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ConfigReader {
     private static HashMap<String, Integer> options = new HashMap<String, Integer>();
 
     public static void readConfig(){
         try {
-            Scanner s = new Scanner(new File("src/config.txt"));
+            Scanner s = new Scanner(new File("config.txt"));
             while (s.hasNextLine()) {
                 String[] line = s.nextLine().split("\\s*=\\s*");
                 options.put(line[0], Integer.parseInt(line[1]));
             }
         }
         catch(FileNotFoundException e){
-            // TODO: Implement default config
+            try {
+                File defaultConfig = new File("config.txt");
+                defaultConfig.createNewFile();
+
+                FileWriter writer = new FileWriter("config.txt");
+                writer.write("width = 600\n");
+                writer.write("height = 400\n");
+                writer.write("guiPadding = 100\n");
+                writer.write("brushSize = 12\n");
+                writer.write("bgColor = 0");
+                writer.close();
+
+                readConfig();
+            } catch (IOException i) {System.out.print(e);}
         }
     }
 
